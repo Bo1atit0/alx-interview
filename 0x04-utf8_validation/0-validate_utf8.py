1aee239f7c1efca5898313a0985b44fb6bc6a320
bo1atito <shorungbebolatito@gmail.com>
2024-11-02 06:31:05 +0100

    pep8

--- 0x04-utf8_validation/0-validate_utf8.py
+++ 0x04-utf8_validation/0-validate_utf8.py
@@ -1,14 +1,15 @@
 #!/usr/bin/python3
 
 """
-Check if the given data set represents 
+Check if the given data set represents
 valid UTF-8 encoding.
 """
+
 
 def validUTF8(data):
 
     """
-    Check if the given data set represents 
+    Check if the given data set represents
     valid UTF-8 encoding.
     """
     bytes_expected = 0
